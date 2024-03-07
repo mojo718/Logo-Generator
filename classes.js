0889906d4132f15c31c29edcb317fc53a188bea7
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-03-06 19:58:43 -0500

    Created classes.js with classes - starting SVG Generator code

--- classes.js
+++ classes.js
@@ -1,3 +1,5 @@
+
+// Created class for Icon
 class Icon {
     constructor (color, shape, text, textColor) {
         this.color = color;
@@ -5,4 +7,39 @@ class Icon {
         this.text = text;
         this.textColor = textColor;
     }
+} 
+//created class for Color to extend Icon
+class Color extends Icon {
+    constructor (color) {
+        super(color, null, null, null)
+    }
 }
+// class for Shape that extends Icon
+class Shape extends Icon {
+    constructor (color) {
+        super(color, null, null, null)
+    }
+}
+// class for Square that extends Shape
+class Square extends Shape {
+    constructor(color) {
+        super("square");
+        this.color = color;
+    }
+}
+// class for Cirle extends Shape
+class Circle extends Shape {
+    constructor(color) {
+        super("circle");
+        this.color = color;
+    }
+}
+// class for Triange extends Shape
+class Triange extends Shape {
+    constructor(color) {
+        super("triange");
+        this.color = color;
+    }
+}
+
+module.exports = classes
