1359332683069e4f5c12bd4fd146a896e30359ed
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-03-07 16:14:24 -0500

    got rid of Classes.js and split it up into shapes, icons, and colors.js

--- logoGenerator.js
+++ logoGenerator.js
@@ -1,61 +1,59 @@
-// import from Classes.js
-const classes = require('./lib/classes');
-// required modules
-const fs = require('fs');
+const path = require('path');
 const inquirer = require('inquirer');
+const fs = require('fs');
+const Icon = require('./lib/icon');
+const { Square, Circle, Triangle } = require('./lib/shapes');
+const Color = require('./lib/colors');
 
-// async function to generate logo with waiting on inquirer answers
-// used switch statement - referenced https://www.w3schools.com/js/js_switch.asp
-
-async function generatelogo() {
+async function generateLogo() {
     const answers = await inquirer.prompt([
         {
             type: 'list',
             name: 'color',
-            message: 'Select color of Logo:',
+            message: 'Select a color:',
             choices: ['red', 'green', 'blue']
         },
         {
             type: 'list',
             name: 'shape',
-            message: 'Select shape of Logo:',
+            message: 'Select a shape:',
             choices: ['square', 'circle', 'triangle']
         },
         {
             type: 'input',
             name: 'text',
-            message: 'Enter text for logo (3 Char Max.):'
+            message: 'Enter text for the logo:'
         },
         {
             type: 'list',
             name: 'textColor',
-            message: 'Select a color for the text',
+            message: 'Select a color for the text:',
             choices: ['red', 'green', 'blue']
-        }]);
+        }
+    ]);
 
-let icon;
-switch (answers.shapes) {
-    case 'square':
-        icon = new Square(answers.color);
-        break;
-    case 'circle':
-        icon = new Circle(answers.color);
-        break;
-    case 'triange':
-        icon = new Triangle(answers.color);
-    default:
-        console.log('Invalid Shape');
-        return
+    let icon;
+    switch (answers.shape) {
+        case 'square':
+            icon = new Icon(new Color(answers.color), new Square(), answers.text, answers.textColor);
+            break;
+        case 'circle':
+            icon = new Icon(new Color(answers.color), new Circle(), answers.text, answers.textColor);
+            break;
+        case 'triangle':
+            icon = new Icon(new Color(answers.color), new Triangle(), answers.text, answers.textColor);
+            break;
+        default:
+            console.log('Invalid shape');
+            return;
+    }
+
+    const svgContent = icon.generateSVG();
+    const fileName = 'logo.svg';
+    const filePath = path.join(__dirname, 'results', fileName); // Results folder
+
+    fs.writeFileSync(filePath, svgContent);
+    console.log(`Logo saved as ${filePath}`);
 }
 
-icon.text = answers.text
-icon.textColor = answers.textColor
-
-const svgContent = icon.generateSVG()
-
-
-
-
-
-
-    }
+generateLogo();
